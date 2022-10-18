152912757c41b19af873ab38cfb09b1da9817bb1
Daniel <Dphillip11@gmail.com>
2022-10-18 11:25:06 +0200

    spikes influence playerY

--- src/Tile.lua
+++ src/Tile.lua
@@ -12,29 +12,30 @@ end
 
 function Tile:collision(object)
     if object.x + object.width - 2 > self.x and object.x + 2< self.x + self.width and object.y + object.height - 2> self.y and object.y + 2< self.y + self.height then
-        self:collisionResponse(object.directionY)
+        self:collisionResponse(object)
         return true
     end
 end
 
-function Tile:collisionResponse(direction)
-    if self.type == '?' and direction == 'N' and self.active == 1 and not self.glideY then 
+function Tile:collisionResponse(object)
+    if self.type == '?' and object.directionY == 'N' and self.active == 1 and not self.glideY then 
         table.insert(level.entities, Entity(self.xIndex,self.yIndex - 1, math.random(-200,200), math.random(-100,-1000)))
         self.glideY = Glide({self.y, self.y - 10, self.y}, 0.3)
         table.insert(level.glides, self.glideY)     
         self.active = 0   
     end
-    if (self.type == 'spikes' or self.type == "bloodySpikes") and direction == 'S' then 
+    if (self.type == 'spikes' or self.type == "bloodySpikes") and object.directionY == 'S' then 
         self.type = 'bloodySpikes'
         playerHealth = math.max(0, playerHealth -1)
+        object.y = object.y - 20
     end
-    if self.type == 'breakBlock3' and direction == 'N' then 
+    if self.type == 'breakBlock3' and object.directionY == 'N' then 
         level.grid[self.xIndex][self.yIndex] = '0'
     end
-    if self.type == 'breakBlock2' and direction == 'N' then 
+    if self.type == 'breakBlock2' and object.directionY == 'N' then 
         self.type = 'breakBlock3'
     end
-    if self.type == 'breakBlock1' and direction == 'N' then 
+    if self.type == 'breakBlock1' and object.directionY == 'N' then 
         self.type = 'breakBlock2'
     end
     
